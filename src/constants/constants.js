242630f13afc9e19d71e801345149837abe343f4
Shrey Koradia <shrey.koradia@yahoo.com>
2024-04-09 09:32:54 +0530

    added new stuff to the highlights

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,55 +1,84 @@
 export const projects = [
   {
-    title: 'UniChat',
-    description: "UniChat Allows to chat with user in one room(Web Socket), The application is built using React.js, Express.js and Web Sockets ",
-      image: '',
-      tags: ['React.js','Express.js','socket.io','Heroku'],
-    source: 'https://github.com/shreykoradia/UniChat',
-    visit: 'https://unichat-v1.herokuapp.com/',
+    title: "RankByte",
+    description:
+      "AI tool to track the college rankings across parameters, patents and infrastructure across all domain of courses.",
+    image: "/images/rankbyte.png",
+    tags: ["JS", "Django", "Three.js"],
+    source: "https://github.com/DedSecdotbat/RankByte-AI-tool",
+    visit: "https://github.com/DedSecdotbat/RankByte-AI-tool",
     id: 0,
   },
+
   {
-    title: 'Covid Tracker',
-    description: "Using React, The App was developed as Design Engineering Project, CovidTracker and it is a simple Covid-Tracker that allows users to keep Track on Covid cases !.",
-      image: '/images/1.png',
-      tags: ['React' , 'Disease.sh API'],
-    source: 'https://github.com/shreykoradia/covidtracker',
-    visit: 'https://covidtrackerbyshrey.herokuapp.com/',
+    title: "UniChat",
+    description:
+      "UniChat Allows to chat with user in one room(Web Socket), The application is built using React.js, Express.js and Web Sockets ",
+    image: "/images/unichat.png",
+    tags: ["React.js", "Express.js", "socket.io", "Heroku"],
+    source: "https://github.com/shreykoradia/UniChat",
+    visit: "https://unichat-v1.herokuapp.com/",
     id: 1,
   },
   {
-    title: 'News AI',
-    description:"News AI worked Backed by Alan AI where a news is Read aloud from any news Source or the internet Influencer or technology or domain ",
-    image: '/images/2.png',
-    tags: ['React', 'JavaScript'],
-    source: 'https://github.com/shreykoradia/NewsAI',
-    visit: 'https://newsai-theta.vercel.app/',
+    title: "Covid Tracker",
+    description:
+      "Using React, The App was developed as Design Engineering Project, CovidTracker and it is a simple Covid-Tracker that allows users to keep Track on Covid cases !.",
+    image: "/images/1.png",
+    tags: ["React", "Disease.sh API"],
+    source: "https://github.com/shreykoradia/covidtracker",
+    visit: "https://covidtrackerbyshrey.herokuapp.com/",
     id: 2,
   },
   {
-    title: 'UnmaskDotPy --CodeStack',
-    description: "A Hackathon gig where me and my team-mates built the platform for Conducting online HAckathons inside a university and conducting code reviews for freshman and sophmore coders",
-      image: '/images/3.png',
-      tags: ['React', 'materialUi core'],
-    source: 'https://github.com/shreykoradia/unmaskdotpy',
-    visit: '#',
+    title: "News AI",
+    description:
+      "News AI worked Backed by Alan AI where a news is Read aloud from any news Source or the internet Influencer or technology or domain ",
+    image: "/images/2.png",
+    tags: ["React", "JavaScript"],
+    source: "https://github.com/shreykoradia/NewsAI",
+    visit: "https://newsai-theta.vercel.app/",
     id: 3,
   },
   {
-    title: 'EmailSender',
-    description: "An application where you can send email across system and platforms. This is a simple application which you can merge in with complex web-application.",
-    image: '/images/4.jpg',
-    tags: ['Express', 'Nodejs'],
-    source: 'https://github.com/shreykoradia/Email-Sender',
-    visit: '#',
+    title: "UnmaskDotPy --CodeStack",
+    description:
+      "A Hackathon gig where me and my team-mates built the platform for Conducting online HAckathons inside a university and conducting code reviews for freshman and sophmore coders",
+    image: "/images/3.png",
+    tags: ["React", "materialUi core"],
+    source: "https://github.com/shreykoradia/unmaskdotpy",
+    visit: "#",
     id: 4,
+  },
+  {
+    title: "EmailSender",
+    description:
+      "An application where you can send email across system and platforms. This is a simple application which you can merge in with complex web-application.",
+    image: "/images/4.jpg",
+    tags: ["Express", "Nodejs"],
+    source: "https://github.com/shreykoradia/Email-Sender",
+    visit: "#",
+    id: 5,
   },
 ];
 
 export const TimeLineData = [
-  { year: 2019, text: 'Started my journey', },
-  { year: 2020, text: 'Started Working on Front-End ', },
-  { year: 2021, text: 'Started working on Back-End and Started participating in Hackathons', },
-  { year: 2022, text: 'Currently Developing Ping-Pong Multiplayer and Hackathon Fleets ', },
-
+  { year: 2019, text: "Started my journey" },
+  { year: 2020, text: "Started Working on Front-End " },
+  {
+    year: 2021,
+    text: "Started working on Back-End and Started participating in Hackathons",
+  },
+  {
+    year: 2022,
+    text: "Currently Developing Ping-Pong Multiplayer and Hackathon Fleets PS: Both of them were archived due to my University Exams",
+  },
+  {
+    year: 2023,
+    text: "Started Working as SDE-1 on a product named Grand which was a Risk Management and Compliance Software, while helping early stage founders build their MVP and scale early users",
+  },
+  {
+    year: 2024,
+    text: "Contributing to Grand as Core Contributor and building Talez my first product solely after wrapping off BingeVoid ( wrapped due to high costs of cloud virtual machines) and Asset Sentinel (MVP for a CISO founder who worked at Blinkit, yahoo, Gojek, Rippling)",
+  },
 ];
